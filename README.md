718fafc474dd65cdf61c7ac27e8396d4535a4863
SeongYun Cho <rekafekili@gmail.com>
2024-07-12 18:39:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fl_music_xml_parser
+ Music XML Parser for Flutter
---
+++ lib/fl_music_xml_parser.dart
@@ -0,0 +1,7 @@
+library fl_music_xml_parser;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
