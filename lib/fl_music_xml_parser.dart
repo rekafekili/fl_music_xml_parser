29d3753a176b3a52f504c1f31f9c841669642208
SeongYun Cho <rekafekili@gmail.com>
2024-08-06 14:52:27 +0900

    Add exports in main

--- lib/fl_music_xml_parser.dart
+++ lib/fl_music_xml_parser.dart
@@ -1,5 +1,14 @@
 library fl_music_xml_parser;
 
+export 'src/data/note_type.dart';
+export 'src/data/stem_value.dart';
+export 'src/data/beam_value.dart';
+export 'src/body/score_part.dart';
+export 'src/header/score_defaults.dart';
+export 'src/header/score_identification.dart';
+export 'src/header/score_part_list.dart';
+export 'src/score_xml.dart';
+
 import 'package:fl_music_xml_parser/src/score_xml.dart';
 import 'package:flutter/services.dart';
 import 'package:xml/xml.dart';
